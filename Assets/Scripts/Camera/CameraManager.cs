6b18c3050eb0ea7b47094073ec9f8ad35b794d45
JesusGarciaUB <jesus.garcia@enti.cat>
2023-02-06 14:01:06 +0100

    GridChange completed

--- Assets/Scripts/Camera/CameraManager.cs
+++ Assets/Scripts/Camera/CameraManager.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
+
+    public float speed = 5.0f;
+    public GameObject target;
     private void LateUpdate()
     {
-        
+        transform.position = Vector3.Lerp(transform.position, target.transform.position, speed * Time.deltaTime);
     }
 }
