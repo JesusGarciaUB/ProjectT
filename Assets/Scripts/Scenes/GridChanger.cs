d9c80a40094fdb81d399c7757108fcb36dac0415
JesusGarciaUB <jesus.garcia@enti.cat>
2023-02-06 11:10:46 +0100

    trigger grid testing ground

--- Assets/Scripts/Scenes/GridChanger.cs
+++ Assets/Scripts/Scenes/GridChanger.cs
@@ -5,14 +5,13 @@ using UnityEngine;
 public class GridChanger : MonoBehaviour
 {
 
-    private Vector3 position;
-    public int speed = 2;
+    private Vector3 position = new Vector3(0,0,0);
     public enum DIRECTION_ENUM { TOP, BOT, LEFT, RIGHT};
     public DIRECTION_ENUM direction;
     public int offset;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Player") collision.transform.position = Vector3.Lerp(collision.transform.position, getPosition(), speed * Time.deltaTime);
+        if (collision.gameObject.tag == "Player") GameObject.FindGameObjectWithTag("Follower").transform.position = getPosition();
     }
 
     Vector3 getPosition()
@@ -20,12 +19,16 @@ public class GridChanger : MonoBehaviour
         switch(direction)
         {
             case DIRECTION_ENUM.TOP:
+                position.y += offset;
                 break;
             case DIRECTION_ENUM.BOT:
+                position.y -= offset;
                 break;
             case DIRECTION_ENUM.LEFT:
+                position.x -= offset;
                 break;
             case DIRECTION_ENUM.RIGHT:
+                position.x += offset;
                 break;
         }
         return position;
