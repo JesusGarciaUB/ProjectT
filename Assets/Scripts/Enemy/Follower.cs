73cbe9eae021857db53fca6b036745a5016111e4
JesusGarciaUB <jesus.garcia@enti.cat>
2023-02-14 12:45:04 +0100

    loot prefab

--- Assets/Scripts/Enemy/Follower.cs
+++ Assets/Scripts/Enemy/Follower.cs
@@ -6,9 +6,12 @@ public class Follower : EnemyBehaviour
 {
     public float speed;
     private int hitcount;
+    public GameObject loot;
     protected void FixedUpdate()
     {
         if (hitcount == 0) transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+        //if (!isAlive) Instantiate(loot, new Vector3(transform.position.x, transform.position.y, 0), Quaternion.identity);
+
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
