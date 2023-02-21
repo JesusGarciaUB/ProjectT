73523955674c6ccf8272866918682aaf4d82a21c
JesusGarciaUB <jesus.garcia@enti.cat>
2023-02-21 08:43:50 +0100

    player health ara si

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -24,13 +24,18 @@ public class PlayerController : MonoBehaviour
     Rigidbody2D rb;
     List<RaycastHit2D> cColl = new List<RaycastHit2D>();
     bool canMove = true;
-    public int health;
     // Start is called before the first frame update
     void Start()
     {
         dir = Direction.DOWN;
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+    }
+
+    public int health
+    {
+        get { return health; }
+        set { health = value; } 
     }
 
     private void FixedUpdate()
