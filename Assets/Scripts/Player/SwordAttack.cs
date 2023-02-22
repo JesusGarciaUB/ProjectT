fc2e21ebe0f330979d8891eefa1b4a8938df8f41
FrancescEnti <francesc.verdugo@enti.cat>
2023-02-22 10:21:50 +0100

    cosa sword attack

--- Assets/Scripts/Player/SwordAttack.cs
+++ Assets/Scripts/Player/SwordAttack.cs
@@ -8,6 +8,7 @@ public class SwordAttack : MonoBehaviour
     Vector2 attackOffset;
     Collider2D swordCollider;
     public int damage = 3;
+    public float knockbackForce = 5000f;
 
     // Start is called before the first frame update
     void Start()
@@ -21,6 +22,7 @@ public class SwordAttack : MonoBehaviour
         swordCollider.enabled = true;
         transform.position = new Vector3(transform.parent.position.x + 0.11f, transform.parent.position.y - 0.06f);
         attackOffset = transform.position;
+
     }
 
     public void AttackLeft()
@@ -51,14 +53,24 @@ public class SwordAttack : MonoBehaviour
         swordCollider.enabled = false;
     }
 
+    private Vector2 Knockback(Collider2D collision)
+    {
+        Vector3 parentPos = gameObject.GetComponentInParent<Transform>().position;
+        Vector2 direction = (Vector2)(collision.gameObject.transform.position - parentPos).normalized;
+        Vector2 knockBack = direction * knockbackForce;
+        print("Collision" + knockBack);
+        return knockBack;
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Enemy")
         {
             EnemyBehaviour enemy = collision.GetComponent<EnemyBehaviour>();
-
+            Rigidbody2D rb = collision.GetComponent<Rigidbody2D>();
             if (enemy != null)
             {
+                rb.AddForce(Knockback(collision));
                 if (enemy.Armor <= 0)
                 {
                     enemy.Health -= damage;
