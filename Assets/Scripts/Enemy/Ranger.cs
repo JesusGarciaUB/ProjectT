6ea4d2fe199ef74b11f5cc4ed6b5c39f00874c36
JesusGarciaUB <jesus.garcia@enti.cat>
2023-02-24 13:17:25 +0100

    ranger shoot start random

--- Assets/Scripts/Enemy/Ranger.cs
+++ Assets/Scripts/Enemy/Ranger.cs
@@ -1,14 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.Random;
 
 public class Ranger : EnemyBehaviour
 {
     public float distanceToPlayer;
     private Vector3 rDirection;
     public float speed;
-
+    public GameObject projectile;
+    public Transform projectilePos;
+    private bool started;
+    private float timeToStart = 0;
+    private float randTime;
     // Update is called once per frame
+
+    new private void Start()
+    {
+        started = false;
+        randTime = Range(1, 3);
+        base.Start();
+    }
+
+    private void Update()
+    {
+        if (!started) timeToStart += Time.deltaTime;
+        if (timeToStart > randTime && !started)
+        {
+            started = true;
+        }
+    }
     void FixedUpdate()
     {
         rDirection = transform.position - player.transform.position;
@@ -20,5 +41,14 @@ public class Ranger : EnemyBehaviour
     public override void Defeated()
     {
         base.Defeated();
+    }
+
+    new private void Attack()
+    {
+        if (canAttack && started)
+        {
+            Instantiate(projectile, projectilePos.position, Quaternion.identity);
+            StartCoroutine(StartCooldown());
+        }
     }
 }
