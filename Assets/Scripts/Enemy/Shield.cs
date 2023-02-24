ff1a524cfa26abcdffe4e3cbd881cb8673776e30
JesusGarciaUB <jesus.garcia@enti.cat>
2023-02-24 12:40:08 +0100

    shield changes color on armor lost

--- Assets/Scripts/Enemy/Shield.cs
+++ Assets/Scripts/Enemy/Shield.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class Shield : EnemyBehaviour
 {
     // Update is called once per frame
+    private bool changed = false;
+    SpriteRenderer sr;
     private void Awake()
     {
+        sr = GetComponent<SpriteRenderer>();
         Armor = 2;
 
         base.Start();
@@ -14,6 +17,15 @@ public class Shield : EnemyBehaviour
     }
 
     public float speed;
+
+    private void Update()
+    {
+        if (Armor <= 0 && !changed)
+        {
+            sr.color = Color.red;
+            changed = true;
+        }
+    }
     protected void FixedUpdate()
     {
         if (!hitting) transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
