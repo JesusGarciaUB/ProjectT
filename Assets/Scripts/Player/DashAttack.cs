39944cd094fc00e68533edcc51492fd67d9aaeb9
Marctupadre <116338801+Marctupadre@users.noreply.github.com>
2023-04-18 10:07:59 +0200

    dash fase 3 hecha
    fefefewfwefewf

--- Assets/Scripts/Player/DashAttack.cs
+++ Assets/Scripts/Player/DashAttack.cs
@@ -7,61 +7,49 @@ public class DashAttack: MonoBehaviour
 {
     float moveInput;
     RaycastHit2D hit;
-    //public LayerMask layerMask;
     int layerMask = 1 << 11;
+    private Vector3 trans;
+    private Vector3 position;
+    private Vector3 lastMovedir;
+    private bool isTouching;
+    private Vector3 actualDirection;
 
-    
     private void Start()
     {
-        Debug.DrawRay(transform.position, Vector3.right * 0.4f, Color.green);
-
-        Debug.DrawRay(transform.position, Vector3.up * 0.4f, Color.green);
-
-        Debug.DrawRay(transform.position, Vector3.down * 0.4f, Color.green);
-
-        Debug.DrawRay(transform.position, Vector3.left * 0.4f, Color.green);
+       
     }
 
   
     void Update()
     {
-        layerMask = LayerMask.GetMask("ObjectCollision");
 
-        RaycastHit2D[] hits;
-        Color rayColor;
-
-        hits = Physics2D.RaycastAll(transform.position, Vector3.right, 0.4f, layerMask);
-        rayColor = Color.green;
-        if(hits.Length > 0)
+        position = PersistentManager.Instance.PlayerGlobal.transform.position;
+        switch (PersistentManager.Instance.PlayerGlobal.GetComponent<PlayerController>().dir) //Check player facing direction to set arrow direction and position
         {
-            rayColor = Color.red;
-        }
-        Debug.DrawRay(transform.position, Vector3.right * 0.4f, rayColor);
+            case PlayerController.Direction.UP:                          //Set arrow facing up
+                actualDirection = Vector3.up;
+                detectWalls();
+                break;
+            case PlayerController.Direction.DOWN:
+                actualDirection = Vector3.down;
+                detectWalls();
+                break;
+            case PlayerController.Direction.LEFT:
+                actualDirection = Vector3.left;
+                detectWalls();
+                break;
+            case PlayerController.Direction.RIGHT:
+                actualDirection = Vector3.right;
+                detectWalls();
+                
+                break;
 
-        hits = Physics2D.RaycastAll(transform.position, Vector3.up, 0.4f, layerMask);
-        rayColor = Color.green;
-        if (hits.Length > 0)
+        }
+        /*private void OnDash()
         {
-            rayColor = Color.red;
+            ;
         }
-        Debug.DrawRay(transform.position, Vector3.up * 0.4f, rayColor);
-
-        hits = Physics2D.RaycastAll(transform.position, Vector3.down, 0.4f, layerMask);
-        rayColor = Color.green;
-        if (hits.Length > 0)
-        {
-            rayColor = Color.red;
-        }
-        Debug.DrawRay(transform.position, Vector3.down * 0.4f, rayColor);
-
-        hits = Physics2D.RaycastAll(transform.position, Vector3.left, 0.4f, layerMask);
-        rayColor = Color.green;
-        if (hits.Length > 0)
-        {
-            rayColor = Color.red;
-        }
-        Debug.DrawRay(transform.position, Vector3.left * 0.4f, rayColor);
-
+        */
         /*
          for (int i = 0; i < hits.Length; i++) //Mirar dentro de los hits
          {
@@ -69,4 +57,30 @@ public class DashAttack: MonoBehaviour
          }
         */
     }
+    private void detectWalls()
+    {
+        layerMask = LayerMask.GetMask("CollisionWall");
+
+        RaycastHit2D[] hits;
+        Color rayColor;
+
+        hits = Physics2D.RaycastAll(transform.position, actualDirection, 0.4f, layerMask);
+        rayColor = Color.green;
+        Debug.DrawRay(transform.position, actualDirection * 0.4f, rayColor);
+        if (hits.Length > 0)
+        {
+            isTouching = true;
+            rayColor = Color.red;
+            Debug.DrawRay(transform.position, actualDirection * 0.4f, rayColor);
+        }
+        else
+        {
+            isTouching = false;
+        }
+    }
+  
+    
+        //float dashDistance = 0.4f;
+        //transform.position += lastMovedir * dashDistance;
+    
 }
