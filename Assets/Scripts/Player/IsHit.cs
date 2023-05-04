b3d4dc6e7135c1cb54fa95b7b72b5a8eff5aa257
JesusGarciaUB <jesus.garcia@enti.cat>
2023-05-04 15:02:35 +0200

    a
    a

--- Assets/Scripts/Player/IsHit.cs
+++ Assets/Scripts/Player/IsHit.cs
@@ -8,14 +8,9 @@ public class IsHit : MonoBehaviour
     public float HitCooldown;
     public int Multiplier;
     public float ticks;
-    private Color OriginalColor;
-    private SpriteRenderer pl;
-    // Start is called before the first frame update
     void Awake()
     {
-        pl = GetComponentInParent<SpriteRenderer>();
         isHit = false;
-        OriginalColor = PersistentManager.Instance.GetOgColor;
     }
 
     public bool Hit
@@ -23,25 +18,9 @@ public class IsHit : MonoBehaviour
         get { return isHit; }
         set { isHit = value; }
     }
-
-    private IEnumerator colorChange()
-    {
-        Color oga = new Color(1, 1, 1, 0.02f);
-        for (int x = 0; x < HitCooldown * Multiplier; x++)
-        {
-            pl.color = oga;
-            yield return new WaitForSeconds(ticks);
-            //pl.color = OriginalColor;
-            yield return new WaitForSeconds(ticks);
-        }
-        //print("done");
-        //yield return null;
-        //pl.color = OriginalColor;
-    }
     public void Hitted()
     {
         StartCoroutine(HitCooldownV());
-        StartCoroutine(colorChange());
     }
 
     public IEnumerator HitCooldownV()
