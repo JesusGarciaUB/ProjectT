9d14a35b888ff4fad0279c3ffe59587d5e9beca5
FrancescEnti <francesc.verdugo@enti.cat>
2023-05-04 15:02:32 +0200

    a

--- Assets/Scripts/Menu/menustart.cs
+++ Assets/Scripts/Menu/menustart.cs
@@ -2,10 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class menustart : MonoBehaviour
 {
     public int scenenum;
+    [SerializeField] AudioMixer myMixer;
+
+    private void Start()
+    {
+        if (PlayerPrefs.HasKey("musicVolume") && myMixer != null)
+        {
+            LoadVolume();
+        }
+    }
+
+    private void LoadVolume()
+    {
+        float cosa1 = PlayerPrefs.GetFloat("musicVolume");
+        float cosa2 = PlayerPrefs.GetFloat("SFXVolume");
+        myMixer.SetFloat("Music", Mathf.Log10(cosa1) * 20);
+        myMixer.SetFloat("SFX", Mathf.Log10(cosa2) * 20);
+    }
+
     public void LoadScene()
     {
         SceneManager.LoadScene(scenenum);
