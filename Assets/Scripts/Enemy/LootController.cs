5e5b91a907fdb07a2eb7b541f9094b1729424c2f
JesusGarciaUB <jesus.garcia@enti.cat>
2023-05-08 10:39:41 +0200

    a
    a

--- Assets/Scripts/Enemy/LootController.cs
+++ Assets/Scripts/Enemy/LootController.cs
@@ -1,12 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class LootController : MonoBehaviour
 {
+    [SerializeField] private GameObject pickup;
+    [SerializeField] private GameObject drop;
+
+    private void Awake()
+    {
+        GameObject sound = Instantiate(drop);
+        Destroy(sound, 2.0f);
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player") {
+            GameObject sound = Instantiate(pickup);
+            Destroy(sound, 2.0f);
             SpriteRenderer sr = GetComponent<SpriteRenderer>();
             if (sr.color == Color.cyan) PersistentManager.Instance.PlayerGlobal.GetComponent<PlayerController>().MagicSetter = PlayerController.MAGICe.ICE;
             if (sr.color == Color.red) PersistentManager.Instance.PlayerGlobal.GetComponent<PlayerController>().MagicSetter = PlayerController.MAGICe.FIRE;
